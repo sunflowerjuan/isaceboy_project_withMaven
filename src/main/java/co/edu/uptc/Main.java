821ad158652053d4c195cf16cbe88b7a96fe1e3a
sunflowersjuan <sebasjuan331@gmail.com>
2025-05-29 03:07:56 -0500

    creation of database connection test

--- src/main/java/co/edu/uptc/Main.java
+++ src/main/java/co/edu/uptc/Main.java
@@ -1,7 +1,9 @@
 package co.edu.uptc;
 
+import co.edu.uptc.persistence.PersistenceController;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new PersistenceController();
     }
 }

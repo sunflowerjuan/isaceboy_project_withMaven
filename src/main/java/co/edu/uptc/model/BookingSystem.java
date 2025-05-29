9199fcb6cd92843e29b99863a175f46a4f0c31f0
sunflowersjuan <sebasjuan331@gmail.com>
2025-05-29 17:01:37 -0500

    finished LOGIC of model

--- src/main/java/co/edu/uptc/model/BookingSystem.java
+++ src/main/java/co/edu/uptc/model/BookingSystem.java
@@ -1,8 +1,131 @@
 package co.edu.uptc.model;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import co.edu.uptc.persistence.controllers.PersistenceController;
 
 public class BookingSystem {
     private List<Booking> bookings;
     private List<Customer> customers;
+    private PersistenceController persistence;
+
+    public BookingSystem() {
+        this.persistence = new PersistenceController();
+        this.bookings = new ArrayList<>();
+        this.customers = new ArrayList<>();
+    }
+
+    // Crud operations for customers
+
+    public boolean createCustomer(Customer customer) {
+        return persistence.createCustomer(customer);
+    }
+
+    public String updateCustomer(Customer customer) {
+        try {
+            persistence.updateCustomer(customer);
+            return "Cliente actualizado correctamente";
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    public Customer findCustomer(String id) {
+        return persistence.findCustomer(id);
+    }
+
+    public boolean deleteCustomer(String id) {
+        try {
+            persistence.deleteCustomer(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public int countCustomers() {
+        return persistence.countCustomers();
+    }
+
+    // Crud operations for Rooms
+    public boolean createRoom(Room room) {
+        return persistence.createRoom(room);
+    }
+
+    public String updateRoom(Room room) {
+        try {
+            persistence.updateRoom(room);
+            return "Habitación actualizada correctamente";
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    public Room findRoom(int id) {
+        return persistence.findRoom(id);
+    }
+
+    public boolean deleteRoom(int id) {
+        try {
+            persistence.deleteRoom(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public List<Room> findAllRooms() {
+        return persistence.findAllRooms();
+    }
+
+    public int countRooms() {
+        return persistence.countRooms();
+    }
+
+    // Crud operations for Bookings
+    public boolean createBooking(Booking booking) {
+        return persistence.createBooking(booking);
+    }
+
+    public String updateBooking(Booking booking) {
+        try {
+            persistence.updateBooking(booking);
+            return "Reserva actualizada correctamente";
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    public Booking findBooking(int id) {
+        return persistence.findBooking(id);
+    }
+
+    public boolean deleteBooking(int id) {
+        try {
+            persistence.deleteBooking(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public List<Booking> findAllBookings() {
+        return persistence.findAllBookings();
+    }
+
+    public List<Booking> getBookingsByRoomType(RoomType roomType) {
+        List<Booking> bookingsByRoomType = new ArrayList<>();
+        for (Booking booking : bookings) {
+            if (booking.getRoom().getRoomType() == roomType) {
+                bookingsByRoomType.add(booking);
+            }
+        }
+        return bookingsByRoomType;
+    }
+
+    public int countBookings() {
+        return persistence.countBookings();
+    }
+
 }

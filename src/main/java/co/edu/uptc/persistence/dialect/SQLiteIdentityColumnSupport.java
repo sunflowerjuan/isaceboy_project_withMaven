a450fe165d81bab54985bf36ab3c776598076912
sunflowersjuan <sebasjuan331@gmail.com>
2025-05-29 13:06:44 -0500

    SQLite dialect

---
+++ src/main/java/co/edu/uptc/persistence/dialect/SQLiteIdentityColumnSupport.java
@@ -0,0 +1,21 @@
+package co.edu.uptc.persistence.dialect;
+
+import org.hibernate.dialect.identity.IdentityColumnSupportImpl;
+
+public class SQLiteIdentityColumnSupport extends IdentityColumnSupportImpl {
+
+    @Override
+    public boolean supportsIdentityColumns() {
+        return true;
+    }
+
+    @Override
+    public String getIdentitySelectString(String table, String column, int type) {
+        return "select last_insert_rowid()";
+    }
+
+    @Override
+    public String getIdentityColumnString(int type) {
+        return "integer primary key autoincrement";
+    }
+}

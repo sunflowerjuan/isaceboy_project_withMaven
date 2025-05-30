78022f4382be6039ef4100ac49ee51646e053a7a
sunflowersjuan <sebasjuan331@gmail.com>
2025-05-30 03:53:16 -0500

    Application FX

--- src/main/java/co/edu/uptc/Main.java
+++ src/main/java/co/edu/uptc/Main.java
@@ -1,9 +1,27 @@
 package co.edu.uptc;
 
-import co.edu.uptc.persistence.controllers.PersistenceController;
+import co.edu.uptc.view.MainView;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        MainView mainView = new MainView();
+        Scene scene = new Scene(mainView.getRoot(), 1200, 800);
+        scene.getStylesheets().add(getClass().getResource("/styles/style.css").toExternalForm());
+
+        primaryStage.setScene(scene);
+        primaryStage.initStyle(StageStyle.UNDECORATED); // Sin bordes
+        primaryStage.setMaximized(true); // Pantalla completa
+        primaryStage.setTitle("IASCEBOY - Sistema Gestor de Reservas");
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        new PersistenceController();
+        launch(args);
     }
 }

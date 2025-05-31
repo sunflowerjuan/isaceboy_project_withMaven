97c82f5febb34f68a3b510d093833691053e4edf
sunflowersjuan <sebasjuan331@gmail.com>
2025-05-31 04:01:20 -0500

    presenter methods

--- src/main/java/co/edu/uptc/presenter/Presenter.java
+++ src/main/java/co/edu/uptc/presenter/Presenter.java
@@ -1,5 +1,6 @@
 package co.edu.uptc.presenter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import co.edu.uptc.model.BookingSystem;
@@ -30,13 +31,57 @@ public class Presenter {
         return bookingSystem.createCustomer(new Customer(id, name, lastName, address, email, phone, 0.0));
     }
 
+    public String updateCustomer(String[] data) {
+        if (data.length < 6) {
+            return "Datos incompletos para actualizar el cliente.";
+        }
+        String id = data[0];
+        String name = data[1];
+        String lastName = data[2];
+        String address = data[3];
+        String email = data[4];
+        String phone = data[5];
+
+        Customer customer = new Customer(id, name, lastName, address, email, phone, 0.0);
+        return bookingSystem.updateCustomer(customer);
+    }
+
+    public boolean deleteCustomer(String id) {
+        return bookingSystem.deleteCustomer(id);
+    }
+
     public List<String[]> findCustomersByIdPartial(String query) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findCustomersByIdPartial'");
+        List<String[]> results = new ArrayList<>();
+        for (Customer customer : bookingSystem.findCustomersByIdPartial(query)) {
+            results.add(new String[] {
+                    customer.getIdentification(),
+                    customer.getName(),
+                    customer.getLastName(),
+                    customer.getAddress(),
+                    customer.getEmail(),
+                    customer.getPhoneNumber()
+            });
+        }
+        return results;
     }
 
     public List<String[]> getAllCustomers() {
+        List<String[]> results = new ArrayList<>();
+        for (Customer customer : bookingSystem.findAllCustomers()) {
+            results.add(new String[] {
+                    customer.getIdentification(),
+                    customer.getName(),
+                    customer.getLastName(),
+                    customer.getAddress(),
+                    customer.getEmail(),
+                    customer.getPhoneNumber()
+            });
+        }
+        return results;
+    }
+
+    public void updateRoomPrice() {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAllCustomers'");
+        throw new UnsupportedOperationException("Unimplemented method 'updateRoomPrice'");
     }
 }
